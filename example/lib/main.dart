3d0ddeb9e3a2040fd78a006ae946195b8cc4b40a
Yasin ilhan <yasinilhan61@gmail.com>
2019-01-01 18:42:05 +0300

    Flutter Pass Code Page Complated

---
+++ example/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lock_screen/flutter_lock_screen.dart';
+
+class PassCodeScreen extends StatefulWidget {
+  PassCodeScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _PassCodeScreenState createState() => new _PassCodeScreenState();
+}
+
+class _PassCodeScreenState extends State<PassCodeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var myPass = [1, 2, 3, 4];
+    return LockScreen(
+        title: "This is Screet",
+        passLength: myPass.length,
+        bgImage: "images/pass_code_bg.jpg",
+        showFingerPass: false,
+        fingerFunction: () => print("dede"),
+        borderColor: Colors.white,
+        showWrongPassDialog: false,
+        wrongPassContent: "Wrong pass please try again.",
+        wrongPassTitle: "Opps!",
+        wrongPassCancelButtonText: "Cancel",
+        codeVerify: (List<int> passcode) async {
+          for (int i = 0; i < myPass.length; i++) {
+            if (passcode[i] != myPass[i]) {
+              return false;
+            }
+          }
+          return true;
+        },
+        onSuccess: () {
+          Navigator.of(context).pushReplacement(
+              new MaterialPageRoute(builder: (BuildContext context) {
+            return null;
+          }));
+        });
+  }
+}
---
+++ lib/flutter_lock_screen.dart
@@ -0,0 +1,487 @@
+library flutter_lock_screen;
+
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+typedef void DeleteCode();
+typedef Future<bool> CodeVerify(List<int> passcode);
+
+class LockScreen extends StatefulWidget {
+  final VoidCallback onSuccess;
+  final VoidCallback fingerFunction;
+  final String title;
+  final int passLength;
+  final bool showWrongPassDialog;
+  final bool showFingerPass;
+  final String wrongPassTitle;
+  final String wrongPassContent;
+  final String wrongPassCancelButtonText;
+  final String bgImage;
+  final Color borderColor;
+  final Color foregroundColor;
+  final CodeVerify codeVerify;
+
+  LockScreen({
+    this.onSuccess,
+    this.title,
+    this.borderColor,
+    this.foregroundColor = Colors.transparent,
+    this.passLength,
+    this.codeVerify,
+    this.fingerFunction,
+    this.showFingerPass = false,
+    this.bgImage,
+    this.showWrongPassDialog = false,
+    this.wrongPassTitle,
+    this.wrongPassContent,
+    this.wrongPassCancelButtonText,
+  })  : assert(title != null),
+        assert(passLength <= 8),
+        assert(bgImage != null),
+        assert(borderColor != null),
+        assert(foregroundColor != null),
+        assert(codeVerify != null),
+        assert(onSuccess != null);
+
+  @override
+  _LockScreenState createState() => _LockScreenState();
+}
+
+class _LockScreenState extends State<LockScreen> {
+  var _currentCodeLength = 0;
+  var _inputCodes = <int>[];
+  var _currentState = 0;
+  Color circleColor = Colors.white;
+
+  _onCodeClick(int code) {
+    if (_currentCodeLength < widget.passLength) {
+      setState(() {
+        _currentCodeLength++;
+        _inputCodes.add(code);
+      });
+
+      if (_currentCodeLength == widget.passLength) {
+        widget.codeVerify(_inputCodes).then((onValue) {
+          if (onValue) {
+            setState(() {
+              _currentState = 1;
+            });
+            widget.onSuccess();
+          } else {
+            _currentState = 2;
+            new Timer(new Duration(milliseconds: 1000), () {
+              setState(() {
+                _currentState = 0;
+                _currentCodeLength = 0;
+                _inputCodes.clear();
+              });
+            });
+            if (widget.showWrongPassDialog) {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Center(
+                      child: AlertDialog(
+                        title: Text(
+                          widget.wrongPassTitle,
+                          style: TextStyle(fontFamily: "Open Sans"),
+                        ),
+                        content: Text(
+                          widget.wrongPassContent,
+                          style: TextStyle(fontFamily: "Open Sans"),
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text(
+                              widget.wrongPassCancelButtonText,
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  });
+            }
+          }
+        });
+      }
+    }
+  }
+
+  _deleteCode() {
+    setState(() {
+      if (_currentCodeLength > 0) {
+        _currentState = 0;
+        _currentCodeLength--;
+        _inputCodes.removeAt(_currentCodeLength);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Stack(
+                      children: <Widget>[
+                        ClipPath(
+                          clipper: BgClipper(),
+                          child: Container(
+                            height: MediaQuery.of(context).size.height,
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage(widget.bgImage),
+                                fit: BoxFit.cover,
+                                colorFilter: ColorFilter.mode(
+                                  Colors.grey.shade800,
+                                  BlendMode.hardLight,
+                                ),
+                              ),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                SizedBox(
+                                  height: Platform.isIOS ? 60 : 30,
+                                ),
+                                Text(
+                                  widget.title,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: "Open Sans"),
+                                ),
+                                SizedBox(
+                                  height: Platform.isIOS ? 50 : 30,
+                                ),
+                                CodePanel(
+                                  codeLength: widget.passLength,
+                                  currentLength: _currentCodeLength,
+                                  borderColor: widget.borderColor,
+                                  foregroundColor: widget.foregroundColor,
+                                  deleteCode: _deleteCode,
+                                  status: _currentState,
+                                ),
+                                SizedBox(
+                                  height: Platform.isIOS ? 30 : 15,
+                                ),
+                                Text(
+                                  "TYPE PASSCODE",
+                                  style: TextStyle(
+                                      color: Colors.white70,
+                                      fontSize: 20,
+                                      fontFamily: "Open Sans"),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        widget.showFingerPass
+                            ? Positioned(
+                                top: MediaQuery.of(context).size.height /
+                                    (Platform.isIOS ? 4 : 5),
+                                left: 20,
+                                bottom: 10,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return Center(
+                                            child: Dialog(
+                                              child: Container(
+                                                height: 400,
+                                                child: Column(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.center,
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.start,
+                                                  children: <Widget>[
+                                                    Padding(
+                                                      padding:
+                                                          const EdgeInsets.only(
+                                                              top: 70.0),
+                                                      child: GestureDetector(
+                                                        onTap: widget
+                                                            .fingerFunction,
+                                                        child: Image.asset(
+                                                          "images/fingerprint.png",
+                                                          height: 100,
+                                                          width: 100,
+                                                          color: Colors.blue,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                    SizedBox(
+                                                      height: 30,
+                                                    ),
+                                                    Text(
+                                                      "Use Touch Id",
+                                                      style: TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.w400,
+                                                          fontSize: 20),
+                                                    ),
+                                                    SizedBox(
+                                                      height: 80,
+                                                    ),
+                                                    Divider(
+                                                      height: 1,
+                                                    ),
+                                                    Padding(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              20.0),
+                                                      child: FlatButton(
+                                                        onPressed: () =>
+                                                            Navigator.pop(
+                                                                context),
+                                                        child: Text(
+                                                          "CANCEL",
+                                                          style: TextStyle(
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .normal,
+                                                              fontSize: 20),
+                                                        ),
+                                                      ),
+                                                    )
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                          );
+                                        });
+                                  },
+                                  child: Image.asset(
+                                    "images/fingerprint.png",
+                                    height: 50,
+                                    width: 50,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              )
+                            : Container(),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: Platform.isIOS ? 5 : 6,
+                  child: Container(
+                    padding: EdgeInsets.only(left: 0, top: 0),
+                    child:
+                        NotificationListener<OverscrollIndicatorNotification>(
+                      onNotification: (overscroll) {
+                        overscroll.disallowGlow();
+                      },
+                      child: GridView.count(
+                        crossAxisCount: 3,
+                        childAspectRatio: 1.6,
+                        mainAxisSpacing: 35,
+                        padding: EdgeInsets.all(8),
+                        children: <Widget>[
+                          buildContainerCircle(1),
+                          buildContainerCircle(2),
+                          buildContainerCircle(3),
+                          buildContainerCircle(4),
+                          buildContainerCircle(5),
+                          buildContainerCircle(6),
+                          buildContainerCircle(7),
+                          buildContainerCircle(8),
+                          buildContainerCircle(9),
+                          Text(""),
+                          buildContainerCircle(0),
+                          buildContainerIcon(),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildContainerCircle(int number) {
+    return InkResponse(
+      highlightColor: Colors.red,
+      onTap: () {
+        _onCodeClick(number);
+      },
+      child: Container(
+        height: 50,
+        width: 50,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10,
+                spreadRadius: 0,
+              )
+            ]),
+        child: Center(
+          child: Text(
+            number.toString(),
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildContainerIcon() {
+    return InkResponse(
+      onTap: () {
+        if (0 < _currentCodeLength) {
+          setState(() {
+            circleColor = Colors.grey.shade300;
+          });
+          Future.delayed(Duration(milliseconds: 200)).then((func) {
+            setState(() {
+              circleColor = Colors.white;
+            });
+          });
+        }
+        _deleteCode();
+      },
+      child: Container(
+        height: 50,
+        width: 50,
+        decoration: BoxDecoration(
+            color: circleColor,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10,
+                spreadRadius: 0,
+              )
+            ]),
+        child: Center(
+          child: Icon(
+            Icons.arrow_back,
+            size: 30,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CodePanel extends StatelessWidget {
+  final codeLength;
+  final currentLength;
+  final borderColor;
+  final foregroundColor;
+  final H = 30.0;
+  final W = 30.0;
+  final DeleteCode deleteCode;
+  final int status;
+  CodePanel(
+      {this.codeLength,
+      this.currentLength,
+      this.borderColor,
+      this.foregroundColor,
+      this.deleteCode,
+      this.status})
+      : assert(codeLength > 0),
+        assert(currentLength >= 0),
+        assert(currentLength <= codeLength),
+        assert(deleteCode != null),
+        assert(status == 0 || status == 1 || status == 2);
+
+  @override
+  Widget build(BuildContext context) {
+    var circles = <Widget>[];
+    var color = borderColor;
+    if (status == 1) {
+      color = Colors.green.shade500;
+    }
+    if (status == 2) {
+      color = Colors.red.shade500;
+    }
+    for (int i = 1; i <= codeLength; i++) {
+      if (i > currentLength) {
+        circles.add(SizedBox(
+            width: W,
+            height: H,
+            child: Container(
+              decoration: new BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: new Border.all(color: color, width: 2.0),
+                  color: foregroundColor),
+            )));
+      } else {
+        circles.add(new SizedBox(
+            width: W,
+            height: H,
+            child: new Container(
+              decoration: new BoxDecoration(
+                shape: BoxShape.circle,
+                border: new Border.all(color: color, width: 1.0),
+                color: color,
+              ),
+            )));
+      }
+    }
+
+    return new SizedBox.fromSize(
+      size: new Size(MediaQuery.of(context).size.width, 30.0),
+      child: new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox.fromSize(
+                size: new Size(40.0 * codeLength, H),
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: circles,
+                )),
+          ]),
+    );
+  }
+}
+
+class BgClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, size.height / 1.5);
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
